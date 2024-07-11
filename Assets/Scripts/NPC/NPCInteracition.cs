515f7c75a79144d9f907048bad319972d7433eb6
MarioLDD <112513242+MarioLDD@users.noreply.github.com>
2024-07-11 18:57:55 -0300

    Dialogue System

---
+++ Assets/Scripts/NPC/NPCInteracition.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class NPCInteracition : MonoBehaviour
+{
+    //[SerializeField] private TMP_Text _NPCText;
+    [SerializeField] private TextNPC[] _NPCText2;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void Conversation()
+    {
+
+    }
+
+
+
+}
+[System.Serializable]
+public class TextNPC
+{
+    public string text;
+
+
+}
